e0004bc878d0068e8dca764756e37651f058bda6
RobertMJin <robert-jin@outlook.com>
2024-02-17 15:20:07 -0500

    Add router for multiple pages

---
+++ frontend/src/LandingPage.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+function LandingPage() {
+    return(
+        <div className="background" >
+          {/* Dots */}
+          <div className="overlap-group">
+            <div className="dotrow">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+            <div className="dotrow">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+            <div className="dotrow">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+          </div>
+
+          {/* Title */}
+          <div className="antic-didone-bolded">Recollect</div>
+          {/* Description */}
+          <div className="antic-didone-regular">actively recall your lectures</div>
+
+          {/* Buttons */}
+          <div className="sign-in">
+            <div className="frame">
+              <button className="button">Sign In</button>
+            </div>
+          </div>
+          <div className="continue-as-guest">
+            <div className="frame">
+              <button className="button">Continue as Guest</button>
+            </div>
+          </div>
+
+          <form action="/action_page.php">
+              <input type="file" id="myFile" name="filename"></input>
+              <input type="submit"></input>
+          </form>
+        </div>
+    )
+}
+
+export default LandingPage;
