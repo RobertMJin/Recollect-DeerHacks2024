20d1da085f6cab3c074723e4f2cffdbb8817542c
TaoEvan <tao.evan.pi@gmail.com>
2024-02-17 15:12:42 -0500

    upload_and_playback

---
+++ frontend/src/Playback.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
+
+const  Playback = () => {
+    const [videoUrl, setVideoUrl] = useState('https://youtube.com/watch?v=ysz5S6PUM-U');
+
+    const handleFileChange = (e) => {
+        setVideoUrl(URL.createObjectURL(e.target.files[0]));
+    };
+
+    return (
+        <div>
+            <h1>Video Player</h1>
+            <input type="file" accept="video/*" onChange={handleFileChange} />
+            {videoUrl && <ReactPlayer url={videoUrl} controls />}
+        </div>
+    )
+};
+
+export default Playback;
