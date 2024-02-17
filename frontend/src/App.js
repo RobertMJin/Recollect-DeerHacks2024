18830934edae309a0fe6c143dd7fcba25f79d9b3
RobertMJin <robert-jin@outlook.com>
2024-02-17 14:02:32 -0500

    add colour

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,46 +3,44 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{background: "linear-gradient(var(--accentcolor-blue100), var(--accentcolor-blue90))"}}>
       <header className="App-header">
-          <div className="top-text">
-
-            {/* Dots */}
-            <div className="overlap-group">
-              <div className="dotrow">
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="dotrow">
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              <div className="dotrow">
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
+        <div className="background" >
+          {/* Dots */}
+          <div className="overlap-group">
+            <div className="dotrow">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
             </div>
-
-            {/* Title */}
-            <div className="antic-didone-bolded">Recollect</div>
-            {/* Description */}
-            <div className="antic-didone-regular">actively recall your lectures</div>
-            
-            {/* Buttons */}
-            <div className="sign-in">
-                <div className="frame">
-                    <button className="button">Sign In</button>
-                </div>
+            <div className="dotrow">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
             </div>
-            <div className="continue-as-guest">
-              <div className="frame">
-                  <button className="button">Continue as Guest</button>
+            <div className="dotrow">
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
             </div>
+          </div>
 
-          </div> 
+          {/* Title */}
+          <div className="antic-didone-bolded">Recollect</div>
+          {/* Description */}
+          <div className="antic-didone-regular">actively recall your lectures</div>
+
+          {/* Buttons */}
+          <div className="sign-in">
+            <div className="frame">
+              <button className="button">Sign In</button>
+            </div>
+          </div>
+          <div className="continue-as-guest">
+            <div className="frame">
+              <button className="button">Continue as Guest</button>
+            </div>
+          </div>
         </div>
       </header>
     </div>
