e1c540ce23debf4e4181ca3bbbbcbdf8e4221877
RobertMJin <robert-jin@outlook.com>
2024-02-17 15:20:07 -0500

    Add router for multiple pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,24 @@
 import './App.css';
-import './singleFileUploader.js';
+import SingleFileUploader from './singleFileUploader.js';
 import React from 'react';
+import LandingPage from "./LandingPage.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App" style={{background: "linear-gradient(var(--accentcolor-blue100), var(--accentcolor-blue90))"}}>
       <header className="App-header">
-        <div className="background" >
-          {/* Dots */}
-          <div className="overlap-group">
-            <div className="dotrow">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="dotrow">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-            <div className="dotrow">
-              <span className="dot"></span>
-              <span className="dot"></span>
-              <span className="dot"></span>
-            </div>
-          </div>
-
-          {/* Title */}
-          <div className="antic-didone-bolded">Recollect</div>
-          {/* Description */}
-          <div className="antic-didone-regular">actively recall your lectures</div>
-
-          {/* Buttons */}
-          <div className="sign-in">
-            <div className="frame">
-              <button className="button">Sign In</button>
-            </div>
-          </div>
-          <div className="continue-as-guest">
-            <div className="frame">
-              <button className="button">Continue as Guest</button>
-            </div>
-          </div>
-
-          <form action="/action_page.php">
-              <input type="file" id="myFile" name="filename"></input>
-              <input type="submit"></input>
-          </form>
-        </div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/fileUpload" element={<SingleFileUploader />} />
+          </Routes>
+            {/*
+            <Route path="/results">
+              <Results />
+            </Route>
+  */}
+        </Router>
       </header>
     </div>
   );
