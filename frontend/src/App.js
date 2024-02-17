62700399e8ecc582f5a2e7fc15866567101f742f
RobertMJin <robert-jin@outlook.com>
2024-02-17 12:40:36 -0500

    edit css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
           <div className="top-text">
+            
+            {/* Dots */}
             <div className="overlap-group">
               <div className="dotrow">
                 <span className="dot"></span>
@@ -22,20 +24,24 @@ function App() {
                 <span className="dot"></span>
               </div>
             </div>
-          </div>
-          <div className="antic-didone-bolded">Recollect</div>
-          <div className="antic-didone-regular">actively recall your lectures</div>
-        <div className="buttons">
+
+            {/* Title */}
+            <div className="antic-didone-bolded">Recollect</div>
+            {/* Description */}
+            <div className="antic-didone-regular">actively recall your lectures</div>
+            
+            {/* Buttons */}
             <div className="sign-in">
                 <div className="frame">
                     <button className="button">Sign In</button>
                 </div>
             </div>
-          <div className="continue-as-guest">
+            <div className="continue-as-guest">
               <div className="frame">
                   <button className="button">Continue as Guest</button>
-              </div>
-          </div>
+            </div>
+
+          </div> 
         </div>
       </header>
     </div>
