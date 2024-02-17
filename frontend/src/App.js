7216773e46328af25e7376ead96edaf8f008c987
RobertMJin <robert-jin@outlook.com>
2024-02-17 11:57:30 -0500

    change html boilercode to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <div className="top-text">
+            <div className="overlap-group">
+              <div className="dotrow">
+                <span className="dot"></span>
+                <span className="dot"></span>
+                <span className="dot"></span>
+              </div>
+              <div className="dotrow">
+                <span className="dot"></span>
+                <span className="dot"></span>
+                <span className="dot"></span>
+              </div>
+              <div className="dotrow">
+                <span className="dot"></span>
+                <span className="dot"></span>
+                <span className="dot"></span>
+              </div>
+            </div>
+          </div>
+          <div className="antic-didone-bolded">Recollect</div>
+          <div className="antic-didone-regular">actively recall your lectures</div>
+        <div className="buttons">
+            <div className="sign-in">
+                <div className="frame">
+                    <button className="button">Sign In</button>
+                </div>
+            </div>
+          <div className="continue-as-guest">
+              <div className="frame">
+                  <button className="button">Continue as Guest</button>
+              </div>
+          </div>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
