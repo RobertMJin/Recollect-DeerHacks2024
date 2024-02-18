3ae768c8202cc2ef34f5d0c969b1479cb575f3a1
TaoEvan <tao.evan.pi@gmail.com>
2024-02-18 02:44:18 -0500

    master_video

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,10 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 from moviepy.video.io.VideoFileClip import VideoFileClip
- 
+from moviepy.editor import concatenate_videoclips
+from stt import transcribe_file
+from summarizer import summarize
+
 # Initializing flask app
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -23,9 +26,11 @@ def upload_file():
         video = request.files['video']
         video.save(os.path.join("./uploads", secure_filename(video.filename)))
         return jsonify({'success': True, 'originalVideoPath':
-                    'uploads/original.mp4'})
+                    'uploads/{video.filename}.mp4'})
 
 
+clips = []
+clipNum = 0
 @app.route('/api/clip', methods=['POST'])
 def clip():
     data = request.get_json()
@@ -39,10 +44,24 @@ def clip():
         return jsonify({'error': 'Invalid request data'}), 400
 
     original_clip = VideoFileClip(original_video_path).subclip(start_time, end_time)
-    clipped_path = 'uploads/clipped.mp4'
+    globals()['clips'].append(original_clip)
+    clipped_path = 'clips/clip' + str(globals()['clipNum']) + '.mp4'
+    globals()['clipNum'] += 1
+
     original_clip.write_videofile(clipped_path, codec='libx264')
 
     return jsonify({'success': True, 'clippedVideoPath': clipped_path})
+
+
+@app.route('/combineclipsvideo', methods=['GET'])
+def combineclipsvideo():
+    clips = globals()['clips']
+
+    final_clip_path = 'final_clips_video.mp4'
+    final = concatenate_videoclips(clips)
+    final.write_videofile(final_clip_path, codec='libx264')
+
+    return send_file(final_clip_path, as_attachment=True)
 
 
 @app.route('/download', methods=['GET'])
@@ -55,6 +74,22 @@ def download():
         return jsonify({'error': 'Invalid request data'}), 400
     
     return send_file(video_path, as_attachment=True)
+
+
+@app.route('/transcript', methods=['GET'])
+def transcript():
+
+    video_path = 'final_clips_video.mp4'
+
+    video = VideoFileClip(video_path)
+
+    audio_path = 'final_clips_audio.wav'
+    video.audio.write_audiofile(audio_path)
+
+    response = transcribe_file(audio_path)
+
+    return send_file(video_path, as_attachment=True)
+
      
 # Running app
 if __name__ == '__main__':
