48157a915b8e1a9c04a619045e91a89042995c29
3kstr3m3 <planetaidan@gmail.com>
2024-02-17 20:43:37 -0500

    added basic speech to text functionality with google speech

---
+++ backend/stt.py
@@ -0,0 +1,34 @@
+import argparse
+import io
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\plane\Documents\DeerHacks2024\Striking_Yen_414621.json"
+from google.cloud import speech
+
+
+def transcribe_file(speech_file: str) -> speech.RecognizeResponse:
+    """Transcribe the given audio file."""
+    client = speech.SpeechClient()
+
+    with open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        # sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(f"Transcript: {result.alternatives[0].transcript}")
+
+    return response
+
+transcribe_file("sample-2.mp3")
+
